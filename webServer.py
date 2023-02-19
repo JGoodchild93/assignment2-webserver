63d8e62a51311760a3c22811dbe13d0f0560678e
JGoodchild <jeg6300@gmail.com>
2023-02-18 21:30:44 -0500

    Code properly displays 404 Error on invalid file name

--- webServer.py
+++ webServer.py
@@ -5,61 +5,64 @@ import sys
 
 
 def webServer(port=13331):
-  serverSocket = socket(AF_INET, SOCK_STREAM)
+    serverSocket = socket(AF_INET, SOCK_STREAM)
 
-  # Prepare a server socket
-  serverSocket.bind(("", port))
+    # Prepare a server socket
+    serverSocket.bind(("127.0.0.1", port))
 
-  # Fill in start
+    # Fill in start
+    serverSocket.listen()
+    # Fill in end
 
-  # Fill in end
+    while True:
+        # Establish the connection
 
-  while True:
-    # Establish the connection
+        print('Ready to serve...')
+        connectionSocket, addr = serverSocket.accept()  # Fill in start -are you accepting connections?     #Fill in end
 
-    print('Ready to serve...')
-    connectionSocket, addr =  # Fill in start -are you accepting connections?     #Fill in end
+        try:
+            message = connectionSocket.recv(1024)  # Fill in start -a client is sending you a message   #Fill in end
+            filename = message.split()[1]
 
-    try:
-      message =  # Fill in start -a client is sending you a message   #Fill in end
-      filename = message.split()[1]
+            # opens the client requested file.
+            # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
 
-      # opens the client requested file.
-      # Plenty of guidance online on how to open and read a file in python. How should you read it though if you plan on sending it through a socket?
-      f = open(filename[1:],  # fill in start #fill in end)
-               # fill in end
+            f = open(filename[1:], "rb")  # fill in start #fill in end
 
-               outputdata=b"Content-Type: text/html; charset=UTF-8\r\n"
-      # Fill in start -This variable can store your headers you want to send for any valid or invalid request.
-      # Content-Type above is an example on how to send a header as bytes
-      # Fill in end
+            outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"
+            # Fill in start -This variable can store your headers you want to send for any valid or invalid request.
+            # Content-Type above is an example on how to send a header as bytes
+            okdata = b"HTTP/1.1 200 OK\r\n"
+            # Fill in end
 
-      # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
-      # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
-      # Fill in start
+            # Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
+            # Note that a complete header must end with a blank line, creating the four-byte sequence "\r\n\r\n" Refer to https://w3.cs.jmu.edu/kirkpams/OpenCSF/Books/csf/html/TCPSockets.html
+            # Fill in start
+            connectionSocket.send(okdata)
+            # Fill in end
 
-      # Fill in end
+            # Send the content of the requested file to the client
+            for i in f:  # for line in file
+                # Fill in start - send your html file contents
+                connectionSocket.send(i)
+                # Fill in end
+            connectionSocket.close()  # closing the connection socket
+        except Exception as e:
+            # Send response message for invalid request due to the file not being found (404)
+            # Fill in start
+            baddata = b"HTTP/1.1 404 Not Found\r\n"
+            connectionSocket.send(baddata)
+            # Fill in end
 
-      # Send the content of the requested file to the client
-      for i in f:  # for line in file
-      # Fill in start - send your html file contents #Fill in end
-        connectionSocket.close()  # closing the connection socket
+            # Close client socket
+            # Fill in start
+            connectionSocket.close()
+            # Fill in end
 
-    except Exception as e:
-  # Send response message for invalid request due to the file not being found (404)
-  # Fill in start
-
-  # Fill in end
-
-  # Close client socket
-  # Fill in start
-
-  # Fill in end
-
-  # Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
-  # serverSocket.close()
-  # sys.exit()  # Terminate the program after sending the corresponding data
+    # Commenting out the below, as its technically not required and some students have moved it erroneously in the While loop. DO NOT DO THAT OR YOURE GONNA HAVE A BAD TIME.
+    # serverSocket.close()
+    # sys.exit()  # Terminate the program after sending the corresponding data
 
 
 if __name__ == "__main__":
-  webServer(13331)
+    webServer(13331)
